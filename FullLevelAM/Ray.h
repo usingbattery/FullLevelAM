04c4a08d80806cc6f19317c8e13b5dd1fbf9be8d
mxy1340829138 <1340829138@qq.com>
2023-04-18 18:44:41 +0800

    Ray.h

---
+++ FullLevelAM/Ray.h
@@ -0,0 +1,14 @@
+#pragma once
+#include"Point3D.h"
+
+class Ray {
+
+
+public:
+	Point3D p;
+	Vector3D v;
+	Ray(Point3D p, Vector3D v) {
+		this->p = p.clone();
+		this->v = v.clone().normalized();
+	}
+};
