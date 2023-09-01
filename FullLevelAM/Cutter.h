0bc5edb915b8f153fb4d1393d93f7207bf9ed020
usingbattery <cuitianle01@126.com>
2023-09-01 19:31:03 +0800

    update

--- FullLevelAM/Cutter.h
+++ FullLevelAM/Cutter.h
@@ -10,28 +10,36 @@ namespace nsp {
 
 		std::vector<Layer> layers;
 
-
 		Cutter(StlModel* stlModel, std::vector<double> heights);
 
 		bool forward();
 
 	private:
 
+		// sort all triangles in stlmodel, by triangle.zMin, from min to max
 		std::vector<SortedTriangle> zMinLowToHigh;
+		// sort all triangles in stlmodel, by triangle.zMaz, from max to min
 		std::vector<SortedTriangle> zMaxHighToLow;
 
+		// the index of the layer in layers, which forward is about to process
 		int curLayerIndex;
 
+		// sort layers by layer.height
 		std::multimap<double, Layer*> sortedLayers;
 
+		// get zMinLowToHigh and zMaxHighToLow
 		void sortTriangles(std::vector<Triangle>* triangles);
 
+		// init layers by heights
 		void initLayers(std::vector<double> heights);
 
+		// get sortedLayers
 		void sortLayers(std::vector<double> heights);
 
+		// get all layer.segement in layers
 		void cut();
 
+		// get all layer.contours in layers
 		void link();
 	};
 }
